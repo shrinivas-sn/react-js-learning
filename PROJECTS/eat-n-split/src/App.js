fbb7515f8c35e7319482449ed3ed7b43d3dcd069
shrinivas-sn <shrinusn2001@gmail.com>
2026-03-11 13:44:13 +0530

    small ui is changed

--- PROJECTS/eat-n-split/src/App.js
+++ PROJECTS/eat-n-split/src/App.js
@@ -36,7 +36,6 @@ function App() {
   }
   return (
     <div className="app">
-      hi
       <FriendsList />
       {showAddFriend && <FormAddFriend />}
       <Button onClick={handleShowAddFriend}>
