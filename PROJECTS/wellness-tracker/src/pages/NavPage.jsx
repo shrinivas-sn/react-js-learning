6b15f98bc7ebdadb81c701ea1706d1a672237aa3
shrinivas-sn <shrinusn2001@gmail.com>
2026-03-16 12:20:29 +0530

    completed home page with some info cards

--- PROJECTS/wellness-tracker/src/pages/NavPage.jsx
+++ PROJECTS/wellness-tracker/src/pages/NavPage.jsx
@@ -2,36 +2,26 @@ import { NavLink } from "react-router-dom";
 import styles from "./NavPage.module.css";
 export default function NavPage() {
   return (
-    <ul className={styles.navbar}>
+    <nav className={styles.navbar}>
       <div>
-        <li>
-          <NavLink className={styles.navbtn} to="/">
-            Home
-          </NavLink>
-        </li>
+        <NavLink className={styles.navbtn} to="/">
+          Home
+        </NavLink>
       </div>
       <div className={styles.navlist}>
-        <li>
-          <NavLink className={styles.navbtn} to="/dashboard">
-            Dashboard
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={styles.navbtn} to="/profile">
-            Profile
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={styles.navbtn} to="/history">
-            History
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={styles.navbtn} to="/logactivity">
-            LogActivity
-          </NavLink>
-        </li>
+        <NavLink className={styles.navbtn} to="/dashboard">
+          Dashboard
+        </NavLink>
+        <NavLink className={styles.navbtn} to="/profile">
+          Profile
+        </NavLink>
+        <NavLink className={styles.navbtn} to="/history">
+          History
+        </NavLink>
+        <NavLink className={styles.navbtn} to="/logactivity">
+          LogActivity
+        </NavLink>
       </div>
-    </ul>
+    </nav>
   );
 }
