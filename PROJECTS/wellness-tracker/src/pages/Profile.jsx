3a2a6035b37dfbe1c0378910e79ac5e61645e207
shrinivas-sn <shrinusn2001@gmail.com>
2026-03-16 18:49:45 +0530

    implemented profile page with profile photo and member since the date of used the app

--- PROJECTS/wellness-tracker/src/pages/Profile.jsx
+++ PROJECTS/wellness-tracker/src/pages/Profile.jsx
@@ -1,5 +1,15 @@
+import styles from "./Profile.module.css";
 export default function Profile() {
-    return(
-        <h1>Profile Page</h1>
-    )
+  const profile = JSON.parse(localStorage.getItem("dailyLog")) || [];
+  console.log(profile);
+  return (
+    <div className={styles.container}>
+      <h1>Profile Page</h1>
+      <div className={styles.profilecard}>
+        <div>{profile[0].name}'s profile details</div>
+        <div className={styles.profilephoto}></div>
+        <div>Member since {profile[0].date}</div>
+      </div>
+    </div>
+  );
 }
